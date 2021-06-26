6fccb09d32e6c21cfcb6a87ad754f7a4b3f086a5
MayconMaiaDev <86531134+MayconMaiaDev@users.noreply.github.com>
2021-06-26 16:47:48 -0300

    Estrutura do projeto'
    Adicionando a estrutura do projeto.

---
+++ Sistema-ERP/java/projeto-ERP/src/main/java/com/example/projetoERP/ProjetoErpApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetoERP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoErpApplication.class, args);
+	}
+
+}
---
+++ Sistema-ERP/java/projeto-ERP/src/test/java/com/example/projetoERP/ProjetoErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetoERP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
